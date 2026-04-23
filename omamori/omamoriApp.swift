7795bdcbc0d242512cf4000cfafba95099673e43
Rick Liu <rickliu1203@gmail.com>
2026-04-23 11:50:50 -0400

    Initial Commit

---
+++ omamori/omamoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  omamoriApp.swift
+//  omamori
+//
+//  Created by bree on 2026-04-23.
+//
+
+import SwiftUI
+
+@main
+struct omamoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ omamoriTests/omamoriTests.swift
@@ -0,0 +1,17 @@
+//
+//  omamoriTests.swift
+//  omamoriTests
+//
+//  Created by bree on 2026-04-23.
+//
+
+import Testing
+@testable import omamori
+
+struct omamoriTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
